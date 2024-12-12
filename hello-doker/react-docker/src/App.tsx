108149b873401634da683502314425bbbc60a13e
Aksshay88 <aksshaybala8846@gmail.com>
2024-12-12 11:17:10 +0530

    📈Docker-containerize

---
+++ hello-doker/react-docker/src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+          this is the sample one where this one used
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>
+        Docker is great and easy to use this is stupid aswin you dont have eyes{" "}
+      </h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        this is the is the asample one where this one
+      </p>
+    </>
+  );
+}
+
+export default App;
